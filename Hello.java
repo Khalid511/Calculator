798e76bb88646209224df4b2217dc4b46d75a3fd
Md. Khalid Hasan <shohag9438@gmail.com>
2021-11-15 21:37:57 +0600

    ADD a print statement

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello{
 
     public static void main(string[] args){
         int result=sum(4, 5);
+        system.out.println("Result = " +result);
     }
 
     static int sum(int a, int b)
